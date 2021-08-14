d1949701c262811b8678a72895879f3dfda143ef
Suryanshomar7240 <suryu7240@gmail.com>
2021-08-15 01:26:15 +0530

    mobile check

--- code/js/nav.js
+++ code/js/nav.js
@@ -7,7 +7,10 @@ closeMenu.addEventListener('click',close);
 
 function show(){
     mainMenu.style.display = 'flex';
-    mainMenu.style.top = '0';
+    mainMenu.style.top = '5%';
+    openMenu.style.display='none';
+    closeMenu.style.display='block';
+    openMenu.style.transition='display 1s ease;';
     // closeMenu.style.display='block';
     // closeMenu.style.top='0';
     // closeMenu.style.right='0';
@@ -15,5 +18,8 @@ function show(){
     // console.log(mainMenu.style);
 }
 function close(){
-    mainMenu.style.top = '-100%';
+    openMenu.style.display='block';
+    closeMenu.style.display='none';
+    openMenu.style.transition='display 1s ease;';
+    mainMenu.style.top = '-110%';
 }
